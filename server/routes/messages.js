b97b25e9fd9d56c8add5b0b9f9b103c0894f0eff
Dylan2137 <waviesteagle@gmail.com>
2026-03-08 23:21:20 +0100

    Tried to fix room creation

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -11,22 +11,31 @@ router.post('/createroom', async(req, res) => {
             res.status(401).send({message: "User not found"});
             console.log("User not found");
         }
-        const [id] = await db.execute("SELECT user_id FROM users WHERE login = ?", [roomLogin])
-        const id2 = id[0].user_id;
-        const [rows] = await db.execute('SELECT * FROM rooms WHERE user1_id = ? AND user2_id = ? OR user2_id = ? AND user1_id = ?', [userId, id2, id2, userId]);
-        if (rows.length === 0){
-            await db.execute('INSERT INTO rooms (user1_id, user2_id) VALUES(?, ?)', [userId, id2])
-            const [roomIdList] = await db.execute('SELECT room_id FROM rooms WHERE user1_id = ? AND user2_id = ? OR user2_id = ? AND user1_id = ?', [userId, id2, id2, userId])
-            const roomId = roomIdList[0].room_id;
-            res.status(200).json({roomId: roomId, message: "Room created successfully."})
-            console.log("Room created")
-        }
         else{
-            const [roomIdList] = await db.execute('SELECT room_id FROM rooms WHERE user1_id = ? AND user2_id = ? OR user2_id = ? AND user1_id = ?', [userId, id2, id2, userId])
-            const roomId = roomIdList[0].room_id;
-            res.status(200).json({roomId: roomId, message: "Room found, putting you into room"})
-            console.log("Room found successfully.")
+            const [id] = await db.execute("SELECT user_id FROM users WHERE login = ?", [roomLogin])
+            const id2 = id[0].user_id;
+            if (id2 === userId){
+                res.status(401).send({message: "Can't create a room with yourself"});
+            }
+            else{
+                const [rows] = await db.execute('SELECT * FROM rooms WHERE (user1_id = ? AND user2_id = ?) OR (user2_id = ? AND user1_id = ?)', [userId, id2, id2, userId]);
+                if (rows.length === 0){
+                    await db.execute('INSERT INTO rooms (user1_id, user2_id) VALUES(?, ?)', [userId, id2])
+                    const [roomIdList] = await db.execute('SELECT room_id FROM rooms WHERE (user1_id = ? AND user2_id = ?) OR (user2_id = ? AND user1_id = ?)', [userId, id2, id2, userId])
+                    const roomId = roomIdList[0].room_id;
+                    res.status(200).json({roomId: roomId, message: "Room created successfully."})
+                    console.log("Room created")
+                }
+                else{
+                    const [roomIdList] = await db.execute('SELECT room_id FROM rooms WHERE (user1_id = ? AND user2_id = ?) OR (user2_id = ? AND user1_id = ?)', [userId, id2, id2, userId])
+                    const roomId = roomIdList[0].room_id;
+                    res.status(200).json({roomId: roomId, message: "Room found, putting you into room"})
+                    console.log("Room found successfully.")
+                }
+            }
+
         }
+
 
     }
     catch(err){
